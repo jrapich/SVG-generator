3456122e63e0777df2fb9ff20f86ed5e18f92165
jeremysr <jeremysr@protonmail.com>
2023-08-27 13:04:28 -0600

    pseudocode for index.js minimum viable product/MVP

---
+++ index.js
@@ -0,0 +1,15 @@
+//inquierer.prompt code here
+
+//inquirer must ask the following:
+    //text to put inside the logo. text must be at least 3 characters
+        //write logic to test if less than 3 characters
+    //text color
+        //must enter a color keyword or a hex color code
+    //shape, either circle, triangle, or square
+        //prompted in list form
+    //the shape's color
+    //specify a name of the SVG file, if left blank, generate default of logo.svg
+
+//once all prompts fulfilled, generate shape into /generated/
+    //call the relevant imported class from /lib/shapes.js
+    //log to console "Generated logo.svg to /generated/"
